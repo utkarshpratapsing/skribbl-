2b0448aece490cacd5231a473feb59bef610ab1c
Parekh <dhairya@cse.iitb.ac.in>
2021-10-09 12:09:03 +0530

    chat app

---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const app = express();
+const socket = require("socket.io");
+const color = require("colors");
+const cors = require("cors");
+const { get_Current_User, user_Disconnect, join_User } = require("./dummyuser");
+
+app.use(express());
+
+const port = 8000;
+
+app.use(cors());
+
+var server = app.listen(
+  port,
+  console.log(
+    `Server is running on the port no: ${(port)} `
+      .green
+  )
+);
+
+const io = socket(server);
+
+//initializing the socket io connection 
+io.on("connection", (socket) => {
+  //for a new user joining the room
+  socket.on("joinRoom", ({ username, roomname }) => {
+    //* create user
+    const p_user = join_User(socket.id, username, roomname);
+    console.log(socket.id, "=id");
+    socket.join(p_user.room);
+
+    //display a welcome message to the user who have joined a room
+    socket.emit("message", {
+      userId: p_user.id,
+      username: p_user.username,
+      text: `Welcome ${p_user.username}`,
+    });
+
+    //displays a joined room message to all other room users except that particular user
+    socket.broadcast.to(p_user.room).emit("message", {
+      userId: p_user.id,
+      username: p_user.username,
+      text: `${p_user.username} has joined the chat`,
+    });
+  });
+
+  //user sending message
+  socket.on("chat", (text) => {
+    //gets the room user and the message sent
+    const p_user = get_Current_User(socket.id);
+
+    io.to(p_user.room).emit("message", {
+      userId: p_user.id,
+      username: p_user.username,
+      text: text,
+    });
+  });
+
+  //when the user exits the room
+  socket.on("disconnect", () => {
+    //the user is deleted from array of users and a left room message displayed
+    const p_user = user_Disconnect(socket.id);
+
+    if (p_user) {
+      io.to(p_user.room).emit("message", {
+        userId: p_user.id,
+        username: p_user.username,
+        text: `${p_user.username} has left the room`,
+      });
+    }
+  });
+});
+
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+const whitelist = ['http://localhost:3000', 'http://localhost:8000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
+
